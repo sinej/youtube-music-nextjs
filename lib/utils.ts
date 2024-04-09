32899fc7fa69abc98c78b259ca96d8398daee207
shin <sinej1020@gmail.com>
2024-04-09 11:29:32 +0900

    더미데이터 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const sleep = (ms: number) => new Promise(res => setTimeout(res, ms))
+export const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
