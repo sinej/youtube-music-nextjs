dd27cb20a62368ae9150cab525f81c7b65944943
shin <sinej1020@gmail.com>
2024-04-09 10:06:36 +0900

    디렉토리 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/src/app/providers/themeProvider";
+import {ThemeProvider} from "@/providers/themeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
